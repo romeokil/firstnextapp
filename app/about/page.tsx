f983961ddeb1c2d53539a666f5e4d01cbc684c63
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-07-23 23:22:08 +0530

    direct img src ko  use krke image ni use kr skte ho saath me next.config.ts me configure krna pdega also <Image> tag me height , width, alt ,src sb dena pdega.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image"
+
 export default function About(){
     return (
         <>
+            <Image
+            src="https://tse3.mm.bing.net/th/id/OIP.U_VJuupQohwnzXcKMztqWgHaEo?pid=Api&P=0&h=180"
+            alt="This is the scenery image"
+            width={30}
+            height={30}
+            />
             <h1>This is the about page</h1>
         </>
     )
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {hostname: "tse3.mm.bing.net"}
+    ]
+  }
 };
 
 export default nextConfig;
