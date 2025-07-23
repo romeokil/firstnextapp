f158240e7261824b99924f6b05ca37507a02cfde
Rahul Kumar <rahulkumarjha58978@gmail.com>
2025-07-23 23:37:13 +0530

    client vs server component until and unless tm "use client" ni likhega wo as a server component hi treat hoga or dusra baat hai ye hai tm kuch client component ka access bhi ni kr paega error phek dega turant

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,9 @@
+"use client"
 export default function Contact(){
     return (
         <>
             <h1>This is the contact section</h1>
+            <button onClick={()=>alert("Bhai contact section ka alert hai hm")}>Click Me</button>
         </>
     )
 }
